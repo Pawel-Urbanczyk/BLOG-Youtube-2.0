8ce220d4289e20932b8c704846ec8a0b2e89fd01
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2017-10-26 14:12:00 +0200

    Dodanie navbara

--- index.php
+++ index.php
@@ -23,7 +23,36 @@
 		
 	</head>
 	<body>
-		
+	
+		<!--Navbar start-->
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<!--header-->
+				<div class="navbar-header">
+					<a href="#" class="navbar-brand">Alternative YouTube</a>
+				</div>
+				
+				<!--search area-->
+				<div class="form-inline nav navbar-nav navbar-right" style="padding: 10px;">
+					<div class="input-group">
+						<input type="search" class="form-control" placeholder="Search something...">
+						
+						<div class="input-group-btn">
+							<button class="btn btn-default"><span style="padding: 3px;" class="glyphicon glyphicon-search"></span></button>
+						</div>
+					</div>
+				</div>
+				
+				<!--menu-->
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">Software</a></li>
+					<li><a href="#">Music</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</nav>
+		<!--Navbar end-->
 		
 	</body>
 </html>
