c1e3fad3d576a8a5dc33bf17e3e7738939218a67
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2017-10-26 13:56:30 +0200

    Dodanie plików do repozytorium

---
+++ index.php
@@ -0,0 +1,29 @@
+<html lang="pl">
+	<head>
+		<title>Youtube 2.0</title>
+		<meta charset="utf-8" />
+		<meta name="description" content="Alternative to YouTube.com" />
+		<meta name="keywords" content="Youtube 2.0" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		
+	<!--Bootstrap links start-->
+	
+		<!--jQuery-->
+		<script	src="js/jquery.js"></script>
+		
+		<!--Latest compiled and minified CSS-->
+		<link rel="stylesheet" href="css/bootstrap.css">
+
+		<!--Latest compiled and minified JavaScript-->
+		<script src="js/bootstrap.js"></script>
+		
+		
+	<!--Bootstrap links end-->
+		
+		
+	</head>
+	<body>
+		
+		
+	</body>
+</html>
