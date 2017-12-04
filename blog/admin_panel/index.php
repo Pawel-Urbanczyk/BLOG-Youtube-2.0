66a4cf89f90b6a5dba5be093ed77e64956fff05e
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2017-12-04 17:14:14 +0100

    Oprogramowanie przycisku edycji w panelu admina.

--- blog/admin_panel/index.php
+++ blog/admin_panel/index.php
@@ -6,6 +6,18 @@
             <script>window.location = 'index.php';</script>
         <?php  }
     }
+
+    if(isset($_POST['edit_submit'])){
+        $blog_title = mysqli_real_escape_string($conn,strip_tags($_POST['blog_title']));
+        $blog_description = mysqli_real_escape_string($conn,strip_tags($_POST['blog_description']));
+        $blog_category = mysqli_real_escape_string($conn,strip_tags($_POST['blog_category']));
+
+
+        $edit_sql = "UPDATE blog_data SET blog_title = '$blog_title', blog_description = '$blog_description', blog_category = '$blog_category' WHERE blog_id = '$_POST[blog_id]'";
+        if(mysqli_query($conn,$edit_sql)){ ?>
+                <script>window.location = 'index.php';</script>
+        <?php }
+    }
 ?>
 <html lang="pl">
 <head>
@@ -65,6 +77,7 @@
             <?php
                 $sql = "SELECT * FROM  blog_data";
                 $run = mysqli_query($conn,$sql);
+                $c = 1;
                 while($rows=mysqli_fetch_assoc($run)){
                     $blog_title = ucwords($rows['blog_title']);
                     $blog_description = substr($rows['blog_description'], 0, 100);
@@ -76,7 +89,7 @@
                     }
                     echo"
                         <tr>
-                            <td>$rows[blog_id]</td>
+                            <td>$c</td>
                             <td>$blog_title</td>
                             <td>$blog_description</td>
                             <td>$rows[blog_author]</td>
@@ -85,15 +98,58 @@
                                 <div class='dropdown'>
                                     <button class='btn btn-primary dropdown-toggle' data-toggle='dropdown'>Actions <span class='caret'></span></button>
                                     <ul class='dropdown-menu'>
-                                        <li><a href='#'>Edit</a></li>
+                                        <li><a href='#edit_modal$c' data-toggle='modal'>Edit</a></li>
                                         <li><a href='index.php?del_id=$rows[blog_id]'>Delete</a></li>
                                         <li><a href='../post.php?post_id=$rows[blog_id]'>View</a></li>
                                     </ul>
                                 </div>
                             </td>
                         </tr>
-                    ";
-                }
+                    "; ?>
+                        <div id='edit_modal<?php echo $c;?>' class='modal fade'>
+                            <div class='modal-dialog'>
+                                <div class='modal-content'>
+                                    <div class='modal-header'>
+                                        <button class='close' data-dismiss='modal'>&times;</button>
+                                       <h4>Modal Header</h4>
+                                    </div>
+                                    <div class='modal-body'>
+                                        <form class='' method='POST' action=''>
+                                            <input type='hidden' name='blog_id' value='<?php echo $rows['blog_id'];?>'>
+                                            <div class='form-group'>
+                                                <label>Title</label>
+                                                <input type='text' class='form-control' name='blog_title' value='<?php echo $blog_title; ?>'>
+                                            </div>
+                                            <div class='form-group'>
+                                                <label>Description</label>
+                                                <input type='text' class='form-control' name='blog_description' value='<?php echo $blog_description; ?>'>
+                                            </div>
+                                            <div class='form-group'>
+                                                <label>Category</label>
+                                                <select name='blog_category' class="form-control">
+                                                    <?php
+                                                        $edit_cat_sql = "SELECT * FROM categories";
+                                                        $edit_cat_run = mysqli_query($conn, $edit_cat_sql);
+                                                        while($edit_cat_rows = mysqli_fetch_assoc($edit_cat_run)){
+                                                            $edit_cat_data = ucwords($edit_cat_rows['cat_data']);
+                                                            if($edit_cat_rows['cat_id'] == $rows['blog_category']){
+                                                                echo "<option value='$edit_cat_rows[cat_id]' selected>$edit_cat_data</option>";
+                                                            }else{
+                                                                echo "<option value='$edit_cat_rows[cat_id]'>$edit_cat_data</option>";
+                                                            }
+                                                        }
+                                                    ?>
+                                                </select>
+                                            </div>
+                                            <div class='form-group'>
+                                                <input type='submit' name='edit_submit' class='btn btn-info btn-block'>
+                                            </div>
+                                        </form>
+                                    <div class='modal-footer'></div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php $c++;}
 
             ?>
 
